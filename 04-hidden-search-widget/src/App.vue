ec133d3c5ec7f220592808c1877ee3171a8a7111
inteltone <inteltone@gmail.com>
2024-02-25 10:42:32 +0300

    other commit

---
+++ 04-hidden-search-widget/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <form>
+	<div class="form-group">
+		<input class="input" :class="{show: isShown}" type="text" placeholder="Поиск...">
+		<button class="btn" @click.prevent="handleBtn"><span class="icon-search"></span></button>
+	</div>
+ </form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			isShown: false,
+		}
+	},
+	methods: {
+		handleBtn() {
+			return this.isShown = !this.isShown
+		}
+	}
+}
+</script>
+
+
