9840604dedeca0ea70e17d3a89be04211bc7dd5f
inteltone <inteltone@gmail.com>
2024-03-01 13:44:08 +0300

    first commit

---
+++ 11-event-keycodes/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<p v-if="!key" class="intro">Нажмите любую клавишу, чтобы получить ее код</p>
+	<div v-else class="container">
+		<div class="item">
+			<p class="title">event.key</p>
+			<div class="code">{{ key }}</div>
+		</div>
+		<div class="item">
+			<p class="title">event.keyCode</p>
+			<div class="code">{{ keyCode }}</div>
+		</div>
+		<div class="item">
+			<p class="title">event.code</p>
+			<div class="code">{{ code }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import { useEventListener } from '@vueuse/core'
+
+export default {
+	data() {
+		return {
+			key: '',
+			keyCode: 0,
+			code: '',
+		}
+	},	
+	mounted() {
+		useEventListener(document, 'keydown', (e) => {
+			this.key = e.key === ' ' ? 'Space' : e.key
+			this.keyCode = e.keyCode
+			this.code = e.code
+		})
+	}	
+}
+</script>
+
+
