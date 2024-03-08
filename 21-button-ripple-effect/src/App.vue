56d8d65c055ec0304049b86c16a711f0d53dec2a
inteltone <inteltone@gmail.com>
2024-03-08 09:14:03 +0300

    first commit

---
+++ 21-button-ripple-effect/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<button class="btn" @click="createCircle">Кликни</button>	
+</template>
+
+<script>
+export default {
+	name: 'App',	
+	methods: {
+		createCircle(e) {
+			const x = e.clientX
+			const y = e.clientY
+			const buttonTop = e.target.offsetTop
+			const buttonLeft = e.target.offsetLeft
+			const xInside = x - buttonLeft
+			const yInside = y - buttonTop
+			let btn = document.querySelector('.btn')
+			let circle = document.createElement('div')
+			circle.classList.add('circle')
+			circle.style.top = yInside + 'px'
+			circle.style.left = xInside + 'px'			
+			btn.appendChild(circle)
+			setTimeout(() => {
+				circle.remove()
+			},500)
+		}
+	},	
+}
+</script>
